42da59720cde8f9f5fff04cab578415773196cbc
Dmitry Kolesov <kolesov.dm@gmail.com>
2019-12-04 13:50:13 +0300

    add url

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Dmitry Kolesov'
 SITENAME = 'Список всего'
-SITEURL = ''
+SITEURL = 'https://kolesovdmitry.github.io/logbook'
 
 PATH = 'content'
 
